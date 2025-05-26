d7775299a4b662ffc71a624fca67431dba311f18
cetaldeve <asbin.dahal4747@gmail.com>
2025-05-25 21:57:19 -0500

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,19 @@
-const isProd = process.env.NODE_ENV === "production";
-const assetPrefix = isProd ? "/aboutme/" : "";
+const isProd = process.env.NODE_ENV === "production"
+const basePath = isProd ? "/aboutme" : ""
+const assetPrefix = isProd ? "/aboutme/" : ""
 
 module.exports = {
   reactStrictMode: true,
-
-  // only add this prefix to your _next/ URLs
+  basePath,
   assetPrefix,
 
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [640,750,828,1080,1200,1920,2048,3840],
     loader: "akamai",
     path: "",
   },
 
-  // expose for your <Avatar src={prefix + "/..."} />
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "" : "",
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
-};
+}
